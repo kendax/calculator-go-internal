0036faf27a78108a98c35f3b4976dd5cf8e705c3
kendax <46243718+kendax@users.noreply.github.com>
2023-11-02 21:15:16 +0300

    Formatting changes

--- controllers/calculator_controller.go
+++ controllers/calculator_controller.go
@@ -213,226 +213,226 @@ func InputSave(c *gin.Context) {
 		}
 
 		switch key {
-		case "modulus":
-			indexToSlice := operatorPosition + 1
-			
-			//If the modulus button has been clicked and the input box is empty, display an empty input box 
-			if len(input) == 0 {
-				input = nil
-				c.Redirect(http.StatusSeeOther, "/")
-			}
-			slicedInput := input[indexToSlice:]                  // Slice the input array just after the operator till the end of the array
-			implodeToPercentage := strings.Join(slicedInput, "") // Join slicedinput's elements to a string
-			toPercentage, err := strconv.ParseFloat(implodeToPercentage, 64) //Convert the string to float64
-			if err != nil {
-				// Handle the error
-				fmt.Println("Error converting to float:", err)
-				input = nil
-				input = append(input, "Error")
-				c.Redirect(http.StatusSeeOther, "/")
-			}
-
-			toPercentage /= 100
-			var operandWithoutModulus []string
-
-			if indexToSlice > 0 {
-				operandWithoutModulus = input[:indexToSlice] // Slice the input array from beginning to and including the operator
-			}
-			firstOperatorPattern := regexp.MustCompile(`(\*\-|\+|-|\*|\/|\(|\))`)
-			secondOperatorPattern := regexp.MustCompile(`(\*|\/|\(|\))`)
-			plusInInput := false
-			divisionInInput := false
-			negativeInInput := false
-			
-			//Iterate over the input slice checking the nature of the operators
-			for _, element := range input {
-				if session.Values["multiAndMinus"] == true {
-					negativeInInput = true
-				} else if secondOperatorPattern.MatchString(element) {
-					divisionInInput = true
-				} else if firstOperatorPattern.MatchString(element) {
-					plusInInput = true
+			case "modulus":
+				indexToSlice := operatorPosition + 1
+				
+				//If the modulus button has been clicked and the input box is empty, display an empty input box 
+				if len(input) == 0 {
+					input = nil
+					c.Redirect(http.StatusSeeOther, "/")
 				}
-			}
-
-			if plusInInput { //If the operator is plus or minus symbol
-				operandWithoutOperator := input[:operatorPosition] // Slice the input array from beginning to just before the operator
-				implodedOperandStr := strings.Join(operandWithoutOperator, "")
-				implodedOperand, err := strconv.ParseFloat(implodedOperandStr, 64)
+				slicedInput := input[indexToSlice:]                  // Slice the input array just after the operator till the end of the array
+				implodeToPercentage := strings.Join(slicedInput, "") // Join slicedinput's elements to a string
+				toPercentage, err := strconv.ParseFloat(implodeToPercentage, 64) //Convert the string to float64
 				if err != nil {
-					// Handle the error, e.g., log it, return an error value, etc.
+					// Handle the error
 					fmt.Println("Error converting to float:", err)
 					input = nil
 					input = append(input, "Error")
 					c.Redirect(http.StatusSeeOther, "/")
 				}
 
-				operandWithModulus := implodedOperand * toPercentage
-				operandWithoutModulus = append(operandWithoutModulus, strconv.FormatFloat(operandWithModulus, 'f', -1, 64))
-				operandWithoutModulusStr := strings.Join(operandWithoutModulus, "")
-				currentValueSpc := addWhitespaceAroundOperators(operandWithoutModulusStr)
-				currentValue, err := CalculateInput(currentValueSpc)
-				if err != nil {
-					fmt.Printf("Error evaluating expression: %v\n", err)
-				} else {
-					fmt.Printf("Result: %.2f\n", currentValue)
-				}
+				toPercentage /= 100
+				var operandWithoutModulus []string
 
-				input = nil // Have the input array being empty
-				input = append(input, strconv.FormatFloat(currentValue, 'f', -1, 64))
-				session.Values["resultDisplayed"] = "yes"
-			} else if divisionInInput { //If the operator is a division or multiplication sign
-				operandWithoutModulus = append(operandWithoutModulus, strconv.FormatFloat(toPercentage, 'f', -1, 64))
-				operandWithoutModulusStr := strings.Join(operandWithoutModulus, "")
-				currentValueSpc := addWhitespaceAroundOperators(operandWithoutModulusStr)
-				currentValue, err := CalculateInput(currentValueSpc)
-				if err != nil {
-					fmt.Printf("Error evaluating expression: %v\n", err)
-				} else {
-					fmt.Printf("Result: %.2f\n", currentValue)
+				if indexToSlice > 0 {
+					operandWithoutModulus = input[:indexToSlice] // Slice the input array from beginning to and including the operator
 				}
+				firstOperatorPattern := regexp.MustCompile(`(\*\-|\+|-|\*|\/|\(|\))`)
+				secondOperatorPattern := regexp.MustCompile(`(\*|\/|\(|\))`)
+				plusInInput := false
+				divisionInInput := false
+				negativeInInput := false
 				
-				input = nil
-				input = append(input, strconv.FormatFloat(currentValue, 'f', -1, 64))
-				session.Values["resultDisplayed"] = "yes"
-			} else if negativeInInput { //If the operators are a multiplication and a minus sign
-				operandWithoutModulus = append(operandWithoutModulus, strconv.FormatFloat(toPercentage, 'f', -1, 64))//Convert to a string before appending to operandWithoutModulus
-				operandWithoutModulusStr := strings.Join(operandWithoutModulus, "")
-				currentValueSpc := addWhitespaceAroundOperators(operandWithoutModulusStr)
-				currentValue, err := CalculateInput(currentValueSpc)
-				if err != nil {
-					fmt.Printf("Error evaluating expression: %v\n", err)
-				} else {
-					fmt.Printf("Result: %.2f\n", currentValue) //Display the number with two decimal places
+				//Iterate over the input slice checking the nature of the operators
+				for _, element := range input {
+					if session.Values["multiAndMinus"] == true {
+						negativeInInput = true
+					} else if secondOperatorPattern.MatchString(element) {
+						divisionInInput = true
+					} else if firstOperatorPattern.MatchString(element) {
+						plusInInput = true
+					}
 				}
 
-				input = nil //Set the slice to empty
-				input = append(input, strconv.FormatFloat(currentValue, 'f', -1, 64))
-				session.Values["resultDisplayed"] = "yes"
-				session.Values["multiAndMinus"] = false
-			} else { // if there is no operator
-				currentValueFlt, err := strconv.ParseFloat(GetInputAsString(input), 64)
-				if err != nil {
-					// Handle the error, e.g., log it, return an error value, etc.
-					fmt.Println("Error converting to float:", err)
-					return
-				}
-
-				currentValueFlt /= 100
-				input = nil
-				input = append(input, strconv.FormatFloat(currentValueFlt, 'f', -1, 64))
-				session.Values["resultDisplayed"] = "yes"
-			}
-
-		case "equals":
-			inputConv := strings.Join(input, "")
-			currentValueSpc := addWhitespaceAroundOperators(inputConv)
-			currentValue, err := CalculateInput(currentValueSpc) //Pass along the values for calculation
-			if err != nil {
-				fmt.Printf("Error evaluating expression: %v\n", err)
-			} else {
-				fmt.Printf("Result: %.2f\n", currentValue)
-			}
-
-			// If the Answer's first value is a period, precede it with a zero
-			if strings.HasPrefix(strconv.FormatFloat(currentValue, 'f', -1, 64), ".") {
-				currentValueStr := "0" + strconv.FormatFloat(currentValue, 'f', -1, 64)
-				currentValueFlt, err := strconv.ParseFloat(currentValueStr, 64)
-				if err != nil {
-					// Handle the error, e.g., log it, return an error value, etc.
-					fmt.Println("Error9 converting to float:", err)
-					return
-				}
-				currentValue = currentValueFlt
-			}
-
-			input = nil
-			input = append(input, strconv.FormatFloat(currentValue, 'f', -1, 64))
-			session.Values["resultDisplayed"] = "yes"
-
-		case "c":
-			input = nil //Have the input slice as empty
-
-		case "delete":
-			//Remove the last element from the input slice
-			if len(input) > 0 {
-				input = input[:len(input)-1]
-			}
-
-		default:
-				var lastElement string
-				var lastElementOperator bool
-				var secondLastElement int
-				var lastElementCountable bool
-
-				if len(input) > 0 {
-					lastElement = input[len(input)-1]
-					lastElementOperator = operatorPattern.MatchString(lastElement)
-					lastElementInt, err := strconv.Atoi(lastElement) //Attempt to convert the last element in the slice to an integer
+				if plusInInput { //If the operator is plus or minus symbol
+					operandWithoutOperator := input[:operatorPosition] // Slice the input array from beginning to just before the operator
+					implodedOperandStr := strings.Join(operandWithoutOperator, "")
+					implodedOperand, err := strconv.ParseFloat(implodedOperandStr, 64)
 					if err != nil {
-						// Handle the error, for example, by returning a default value or logging the error.
-						fmt.Println("Error:", err)
+						// Handle the error, e.g., log it, return an error value, etc.
+						fmt.Println("Error converting to float:", err)
+						input = nil
+						input = append(input, "Error")
+						c.Redirect(http.StatusSeeOther, "/")
 					}
-					lastElementCountable = canBeCounted(lastElementInt)
-				}
-				if len(input) > 2 {
-					secondLastElement = len(input) - 2
-				}
-				secondLastMultiply := false
 
-				if secondLastElement != 0 {
-					if input[secondLastElement] == "*" {
-						secondLastMultiply = true
+					operandWithModulus := implodedOperand * toPercentage
+					operandWithoutModulus = append(operandWithoutModulus, strconv.FormatFloat(operandWithModulus, 'f', -1, 64))
+					operandWithoutModulusStr := strings.Join(operandWithoutModulus, "")
+					currentValueSpc := addWhitespaceAroundOperators(operandWithoutModulusStr)
+					currentValue, err := CalculateInput(currentValueSpc)
+					if err != nil {
+						fmt.Printf("Error evaluating expression: %v\n", err)
+					} else {
+						fmt.Printf("Result: %.2f\n", currentValue)
 					}
-				}
-				//Check if there is a period in the first operand
-				if foundOperator == false && periodInInput && key == "period" {
-					periodInOperandOne = true
-				}
-				//If the period button has been clicked and there is no number preceeding it, append a zero before the period
-				if key == "period" && (lastElementCountable == false || len(input) < 1 || lastElementOperator) {
-					input = append(input, "0")
-				}
-				var valueIsMinus bool
-				
-				//Check if the button pressed is the minus button
-				for _, singleValue := range value {
-					if singleValue == "-" {
-						// Do something when "-" is found in the slice
-						valueIsMinus = true
+
+					input = nil // Have the input array being empty
+					input = append(input, strconv.FormatFloat(currentValue, 'f', -1, 64))
+					session.Values["resultDisplayed"] = "yes"
+				} else if divisionInInput { //If the operator is a division or multiplication sign
+					operandWithoutModulus = append(operandWithoutModulus, strconv.FormatFloat(toPercentage, 'f', -1, 64))
+					operandWithoutModulusStr := strings.Join(operandWithoutModulus, "")
+					currentValueSpc := addWhitespaceAroundOperators(operandWithoutModulusStr)
+					currentValue, err := CalculateInput(currentValueSpc)
+					if err != nil {
+						fmt.Printf("Error evaluating expression: %v\n", err)
+					} else {
+						fmt.Printf("Result: %.2f\n", currentValue)
 					}
-				}
-			
-				if (key == "divide" || key == "multiply" || key == "add") && valueIsOperator && len(input) < 1 {/*Prohibit an operator
-					being provided as the first value in the input box*/
+					
 					input = nil
-				} else if lastElement == "*" && valueIsMinus { //Allow the minus operator to follow the multiplication operator
-					session.Values["multiAndMinus"] = true
-					input = append(input, value...) //Append the value slice to the input slice
-				} else if secondLastMultiply && lastElement == "-" && valueIsOperator { /*If any operator comes immediately after
-					  a multiplication sign and a minus sign, remove the latter two and insert that operator into the input box */
-					if len(input) >= 2 {
-						input = input[:len(input)-2]
+					input = append(input, strconv.FormatFloat(currentValue, 'f', -1, 64))
+					session.Values["resultDisplayed"] = "yes"
+				} else if negativeInInput { //If the operators are a multiplication and a minus sign
+					operandWithoutModulus = append(operandWithoutModulus, strconv.FormatFloat(toPercentage, 'f', -1, 64))//Convert to a string before appending to operandWithoutModulus
+					operandWithoutModulusStr := strings.Join(operandWithoutModulus, "")
+					currentValueSpc := addWhitespaceAroundOperators(operandWithoutModulusStr)
+					currentValue, err := CalculateInput(currentValueSpc)
+					if err != nil {
+						fmt.Printf("Error evaluating expression: %v\n", err)
+					} else {
+						fmt.Printf("Result: %.2f\n", currentValue) //Display the number with two decimal places
 					}
-					input = append(input, value...)
-				} else if valueIsOperator && lastElementOperator { //Prevent two operators from following each other
+
+					input = nil //Set the slice to empty
+					input = append(input, strconv.FormatFloat(currentValue, 'f', -1, 64))
+					session.Values["resultDisplayed"] = "yes"
+					session.Values["multiAndMinus"] = false
+				} else { // if there is no operator
+					currentValueFlt, err := strconv.ParseFloat(GetInputAsString(input), 64)
+					if err != nil {
+						// Handle the error, e.g., log it, return an error value, etc.
+						fmt.Println("Error converting to float:", err)
+						return
+					}
+
+					currentValueFlt /= 100
+					input = nil
+					input = append(input, strconv.FormatFloat(currentValueFlt, 'f', -1, 64))
+					session.Values["resultDisplayed"] = "yes"
+				}
+
+			case "equals":
+				inputConv := strings.Join(input, "")
+				currentValueSpc := addWhitespaceAroundOperators(inputConv)
+				currentValue, err := CalculateInput(currentValueSpc) //Pass along the values for calculation
+				if err != nil {
+					fmt.Printf("Error evaluating expression: %v\n", err)
+				} else {
+					fmt.Printf("Result: %.2f\n", currentValue)
+				}
+
+				// If the Answer's first value is a period, precede it with a zero
+				if strings.HasPrefix(strconv.FormatFloat(currentValue, 'f', -1, 64), ".") {
+					currentValueStr := "0" + strconv.FormatFloat(currentValue, 'f', -1, 64)
+					currentValueFlt, err := strconv.ParseFloat(currentValueStr, 64)
+					if err != nil {
+						// Handle the error, e.g., log it, return an error value, etc.
+						fmt.Println("Error9 converting to float:", err)
+						return
+					}
+					currentValue = currentValueFlt
+				}
+
+				input = nil
+				input = append(input, strconv.FormatFloat(currentValue, 'f', -1, 64))
+				session.Values["resultDisplayed"] = "yes"
+
+			case "c":
+				input = nil //Have the input slice as empty
+
+			case "delete":
+				//Remove the last element from the input slice
+				if len(input) > 0 {
+					input = input[:len(input)-1]
+				}
+
+			default:
+					var lastElement string
+					var lastElementOperator bool
+					var secondLastElement int
+					var lastElementCountable bool
+
 					if len(input) > 0 {
-						input = input[:len(input)-1]
+						lastElement = input[len(input)-1]
+						lastElementOperator = operatorPattern.MatchString(lastElement)
+						lastElementInt, err := strconv.Atoi(lastElement) //Attempt to convert the last element in the slice to an integer
+						if err != nil {
+							// Handle the error, for example, by returning a default value or logging the error.
+							fmt.Println("Error:", err)
+						}
+						lastElementCountable = canBeCounted(lastElementInt)
 					}
-					input = append(input, value...)
-				} else if session.Values["isOperator"] == true { /*If an operator is clicked while the input box is displaying an answer
-					  use that answer for subsequent calculations as the first operand */
-					input = append(input, value...)
-				} else if session.Values["resultDisplayed"] == "yes" { /*If any number is clicked while the input box is displaying an
-					  answer, erase and start inserting values afresh */
-					input = nil
-					input = append(input, value...)
-					session.Values["resultDisplayed"] = "no"
-				} else if periodInOperandOne || (periodAfterOperator && key == "period") { //Prevent an operand from having more than one period
-					input = input
-				} else { //Add the value of the clicked button to the input array
-					input = append(input, value...)
-				}
+					if len(input) > 2 {
+						secondLastElement = len(input) - 2
+					}
+					secondLastMultiply := false
+
+					if secondLastElement != 0 {
+						if input[secondLastElement] == "*" {
+							secondLastMultiply = true
+						}
+					}
+					//Check if there is a period in the first operand
+					if foundOperator == false && periodInInput && key == "period" {
+						periodInOperandOne = true
+					}
+					//If the period button has been clicked and there is no number preceeding it, append a zero before the period
+					if key == "period" && (lastElementCountable == false || len(input) < 1 || lastElementOperator) {
+						input = append(input, "0")
+					}
+					var valueIsMinus bool
+					
+					//Check if the button pressed is the minus button
+					for _, singleValue := range value {
+						if singleValue == "-" {
+							// Do something when "-" is found in the slice
+							valueIsMinus = true
+						}
+					}
+				
+					if (key == "divide" || key == "multiply" || key == "add") && valueIsOperator && len(input) < 1 {/*Prohibit an operator
+						being provided as the first value in the input box*/
+						input = nil
+					} else if lastElement == "*" && valueIsMinus { //Allow the minus operator to follow the multiplication operator
+						session.Values["multiAndMinus"] = true
+						input = append(input, value...) //Append the value slice to the input slice
+					} else if secondLastMultiply && lastElement == "-" && valueIsOperator { /*If any operator comes immediately after
+						a multiplication sign and a minus sign, remove the latter two and insert that operator into the input box */
+						if len(input) >= 2 {
+							input = input[:len(input)-2]
+						}
+						input = append(input, value...)
+					} else if valueIsOperator && lastElementOperator { //Prevent two operators from following each other
+						if len(input) > 0 {
+							input = input[:len(input)-1]
+						}
+						input = append(input, value...)
+					} else if session.Values["isOperator"] == true { /*If an operator is clicked while the input box is displaying an answer
+						use that answer for subsequent calculations as the first operand */
+						input = append(input, value...)
+					} else if session.Values["resultDisplayed"] == "yes" { /*If any number is clicked while the input box is displaying an
+						answer, erase and start inserting values afresh */
+						input = nil
+						input = append(input, value...)
+						session.Values["resultDisplayed"] = "no"
+					} else if periodInOperandOne || (periodAfterOperator && key == "period") { //Prevent an operand from having more than one period
+						input = input
+					} else { //Add the value of the clicked button to the input array
+						input = append(input, value...)
+					}
 		}
 	}
 	session.Save(c.Request, c.Writer) //Save session data 
